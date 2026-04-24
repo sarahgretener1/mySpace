209ea7a9aa62e802134b56335dec396779a39d67
sarahgretener1 <sarah.gretener@stud.hslu.ch>
2026-04-24 12:36:33 +0200

    firebase

--- firebase-config.js
+++ firebase-config.js
@@ -15,12 +15,20 @@
  * Ohne gültige Konfiguration läuft die Synchronisation automatisch nur
  * über den BroadcastChannel-Fallback (funktioniert im selben Browser).
  */
-const FIREBASE_CONFIG = {
-  apiKey: "YOUR_FIREBASE_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
+
+ // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBm_owScyd9m98cXDcAaHsexehVI0qIvlc",
+    authDomain: "myspace-room.firebaseapp.com",
+    databaseURL: "https://myspace-room-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "myspace-room",
+    storageBucket: "myspace-room.firebasestorage.app",
+    messagingSenderId: "660441624044",
+    appId: "1:660441624044:web:10b98f67a4d9f74ca4e4c9"
+  };
+
